d450d9622302e1119429b362bdb6c09d8f7b46a0
zele26 <zelalemfiseha26@gmail.com>
2026-07-08 15:04:46 +0000

    Make greeting more casual

--- sample-app/index.js
+++ sample-app/index.js
@@ -22,7 +22,7 @@ const APP_VERSION = process.env.APP_VERSION || "1.0.0";
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from the DevOps Workshop app!",
+    message: "Hey there! Welcome to the DevOps workshop app!",
     version: APP_VERSION,
     servedBy: os.hostname(), // useful later to SEE load-balancing across pods
     timestamp: new Date().toISOString(),
